22aadc447286d3245d7f23bf93aa516dcc11aa65
AkihiroAsai <akihiro_asai@jp.ricoh.com>
2024-06-17 15:48:42 +0900

    pointer access

--- test.c
+++ test.c
@@ -10,10 +10,15 @@ static STest S_test;
 
 int main(void)
 {
-	STest *p_test = &S_test;
+	unsigned char *p_test = (unsigned char *)&S_test;
 	S_test.head = 12345;
 	S_test.mid = 6;
 	S_test.tail = 78;
-	printf("%u %u %u\n", p_test->head, p_test->mid, p_test->tail);
+
+	*p_test = 1;
+	*(++p_test) = 2;
+	*(++p_test) = 3;
+	*(++p_test) = 4;
+	printf("%u %u %u\n", S_test.head, S_test.mid, S_test.tail);
 	return 0;
 }
