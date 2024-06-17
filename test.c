8b411dc0fc2c8e3f335581b0d39079ef813b71b4
AkihiroAsai <akihiro_asai@jp.ricoh.com>
2024-06-17 15:57:24 +0900

    array

--- test.c
+++ test.c
@@ -6,19 +6,25 @@ typedef struct {
 	unsigned char tail;
 } STest;
 
-static STest S_test;
+static STest S_test[2];
 
 int main(void)
 {
 	unsigned char *p_test = (unsigned char *)&S_test;
-	S_test.head = 12345;
-	S_test.mid = 6;
-	S_test.tail = 78;
+	S_test[0].head = 12345;
+	S_test[0].mid = 6;
+	S_test[0].tail = 78;
+	S_test[1].head = 222;
+	S_test[1].mid = 33;
+	S_test[1].tail = 44;
 
-	*p_test = 1;
-	*(++p_test) = 2;
-	*(++p_test) = 3;
-	*(++p_test) = 4;
-	printf("%u %u %u\n", S_test.head, S_test.mid, S_test.tail);
+	for (int i=0; i<8; i++)
+	{
+		*(p_test + i) = i;
+	}
+	for (int i=0; i<2; i++)
+	{
+		printf("%u %u %u\n", S_test[i].head, S_test[i].mid, S_test[i].tail);
+	}
 	return 0;
 }
