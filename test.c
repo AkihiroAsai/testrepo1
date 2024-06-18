dd86dbe817924f42fbd4dfbb37eb68cbd6d2cd83
AkihiroAsai <akihiro_asai@jp.ricoh.com>
2024-06-18 09:57:38 +0900

    add frome https://jvndb.jvn.jp/ja/cwe/CWE-119.html

--- test.c
+++ test.c
@@ -12,3 +12,16 @@ memcpy(buf, argv[1], 5012);
 printWrapper(argv[1]);
 return (0);
 }
+
+void host_lookup(char *user_supplied_addr){
+struct hostent *hp;
+in_addr_t *addr;
+char hostname[64];
+in_addr_t inet_addr(const char *cp);
+
+/*routine that ensures user_supplied_addr is in the right format for conversion */
+validate_addr_form(user_supplied_addr);
+addr = inet_addr(user_supplied_addr);
+hp = gethostbyaddr( addr, sizeof(struct in_addr), AF_INET);
+strcpy(hostname, hp->h_name);
+}
