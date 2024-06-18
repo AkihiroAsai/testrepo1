959f04f18543ad493f1df3d4f9bdcff53729989d
AkihiroAsai <akihiro_asai@jp.ricoh.com>
2024-06-18 10:55:12 +0900

    call main_1 from main

--- test.c
+++ test.c
@@ -11,11 +11,12 @@ int main(int argc, char **argv) {
 char buf[5012];
 memcpy(buf, argv[1], 5012);
 printWrapper(argv[1]);
-return (0);
+return (main_1(argc,argv));
 }
 
 int main_1(int argc, char **argv) {
 char *items[] = {"boat", "car", "truck", "train"};
 int index = argv[1];
 printf("You selected %s¥n", items[index-1]);
+return 0;
 }
